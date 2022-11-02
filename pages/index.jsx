d8560af4feea7e4c616d426d0c9e1a3136f2c53d
noeykan <natdanai200@gmail.com>
2022-11-02 21:25:41 +0700

    done
    done

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,84 +1,154 @@
-import { NFTCard } from "./nftCard"
-import { useState } from 'react'
- 
+
+
+import { useState, useEffect } from 'react';
+import { NFTCard } from "./nftCard";
+import PaginationBar from "./Pagination";
 const Home = () => {
- const [wallet, setWalletAddress] = useState("");
- const [collection, setCollectionAddress] = useState("");
- const [NFTs, setNFTs] = useState([])
- const [fetchForCollection, setFetchForCollection]=useState(false)
+  const [wallet, setWalletAddress] = useState("");
+  const [collection, setCollectionAddress] = useState("");
+  const [nfts, setNFTs] = useState([]);
+  const [fetchForCollection, setFetchForCollection] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pageKeys, setPageKeys] = useState([""]);
+  const API_KEY = "XwECHjdJT_Nlpkwhy29S1DaE6Pa6n0Ab";
 
 
-  const fetchNFTs = async() => {
-    let nfts; 
-    console.log("fetching nfts");
-    const api_key = "XwECHjdJT_Nlpkwhy29S1DaE6Pa6n0Ab"
-    const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
-    var requestOptions = {
+  async function fetchNFTs() {
+    let NFTs;
+    console.log("Fetching NFTs...");
+
+    const apiKey = API_KEY;
+    const baseURL = `https://eth-mainnet.alchemyapi.io/nft/v2/${apiKey}/getNFTs/`;
+
+    if (!collection.length) {
+      var requestOptions = {
         method: 'GET'
       };
-     
-    if (!collection.length) {
-    
+
       const fetchURL = `${baseURL}?owner=${wallet}`;
-  
-      nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+      NFTs = await fetch(fetchURL, requestOptions).then(data => data.json());
     } else {
-      console.log("fetching nfts for collection owned by address")
+      console.log("Fetching Nfts from collection that are owned by the given address...");
+
       const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}`;
-      nfts= await fetch(fetchURL, requestOptions).then(data => data.json())
+      NFTs = await fetch(fetchURL, requestOptions).then(data => data.json());
     }
-  
-    if (nfts) {
-      console.log("nfts:", nfts)
-      setNFTs(nfts.ownedNfts)
+
+    if (NFTs) {
+      console.log("Nfts owned: ", NFTs);
+      const animals = [];
+
+   
+      if(NFTs.ownedNfts.length > 10){
+       
+        for(var i=0;i<NFTs.ownedNfts.length;i++){
+            if(i%5==0){
+              animals.push(NFTs.ownedNfts[i]);
+            }
+        }
+        console.log(animals);
+      }
+      setNFTs(NFTs.ownedNfts);
     }
   }
-  
-  const fetchNFTsForCollection = async () => {
+
+  async function fetchNFTsForCollection(startToken = "", pageIndex = 0) {
+
     if (collection.length) {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = "XwECHjdJT_Nlpkwhy29S1DaE6Pa6n0Ab"
-      const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
-      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
-      const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
-      if (nfts) {
-        console.log("NFTs in collection:", nfts)
-        setNFTs(nfts.nfts)
+      const apiKey = "XwECHjdJT_Nlpkwhy29S1DaE6Pa6n0Ab";
+
+      const baseURL = `https://eth-mainnet.alchemyapi.io/nft/v2/${apiKey}/getNFTsForCollection/`;
+      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}&startToken=${startToken}`;
+      const NFTs = await fetch(fetchURL, requestOptions).then(data => data.json());
+
+      if (NFTs) {
+        if (NFTs.nextToken) {
+          setPageKeys((prevKeys) => {
+            const newKeys = [...prevKeys];
+            newKeys[pageIndex + 1] = NFTs.nextToken;
+
+            return newKeys;
+          });
+        }
+        setNFTs(NFTs.nfts);
       }
     }
   }
+  const onClickPage = (e, pageIndex) => {
+    if (currentPage === pageIndex) return;
 
- return (
-  
-   <div className="flex flex-col items-center justify-center py-8 gap-y-3  bg-blue-400" >
-     <img 
-            src="https://sv1.img.in.th/eCzzX.png" alt="mosutk logo" style={{width: '300px', height: '300px'}}/>
-     <div className="flex flex-col w-full justify-center items-center gap-y-2">
-     <input disabled={fetchForCollection} type={"text"} placeholder="Add your wallet address" 
-     onChange={e => setWalletAddress(e.target.value)} value={wallet}  style={{width: '500px', height: '50px'}}></input>
-       <input type={"text"} placeholder="Add the collection address" style={{width: '500px', height: '50px'}}></input>
-       <label className="text-gray-600"><input onChange={(e)=>{setFetchForCollection(e.target.checked)}} type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
-       <button className={"disabled:bg-slate-500 text-white bg-pink-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
-          () => {
-           if (fetchForCollection) {
-             fetchNFTsForCollection()
-           }else fetchNFTs()
-         }
-       }>Let's go! </button>
-     </div>
-     <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
-       {
-         NFTs.length && NFTs.map(nft => {
-           return (
-             <NFTCard nft={nft}></NFTCard>
-           )
-         })
-       }
-     </div>
-   </div>
- )
+    try {
+      fetchNFTsForCollection(pageKeys[pageIndex], pageIndex);
+      setCurrentPage(pageIndex);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  function nextxxx(e) {
+    e.preventDefault();
+    
+    console.log('You clicked submit.'+nfts[0].description);
+  }
+
+  return (
+    <div className="bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-green-400 to-indigo-900 h-screen w-screen">
+      <center><img src="https://sv1.img.in.th/eCzzX.png" alt="mosutk logo" style={{ width: '300px', height: '300px' }} /></center>
+      <div className="bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-green-400 to-indigo-900 flex flex-col items-center justify-center py-8 gap-y-3">
+
+        <div className="flex flex-col w-full justify-center items-center gap-y-2">
+
+          <label><input onChange={(e) => { setFetchForCollection(e.target.checked) }} type={"checkbox"}></input><a className='text-zinc-50	'> Fetching for a collection </a></label>
+
+          <input disabled={fetchForCollection} className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-grey-800 focus:outline-purple-300 disabled:outline-none disabled:bg-transparent disabled:text-opacity-0" onChange={(e) => { setWalletAddress(e.target.value) }} value={wallet} type={"text"} placeholder="Add Wallet Address!"></input>
+
+          <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-grey-800 focus:outline-purple-300 disabled:bg-slate-50 disabled:text-grey-50" onChange={(e) => { setCollectionAddress(e.target.value) }} value={collection} type={"text"} placeholder="Add Collection Address!"></input>
+
+
+          <button className={"shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white hover:font-bold py-2 px-4 rounded"} onClick={
+            () => {
+              if (fetchForCollection) {
+                fetchNFTsForCollection();
+              }
+              else {
+                fetchNFTs();
+              }
+            }
+          }>Load NFTs !!</button>
+
+        </div>
+        <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-3 justify-center' style={{ width: '1000px', }} >
+          {console.log("pageKeys" + pageKeys)} {
+
+   <button onClick={nextxxx} className={"shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white hover:font-bold py-2 px-4 rounded"} 
+  >{">>>>>>>>>>"}</button>
+            
+            
+          }
+        </div>  
+        <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-3 justify-center' style={{ width: '1000px', }} >
+          {console.log("pageKeys" + pageKeys)} {
+
+            nfts.length && nfts.map(nft => {
+              return (
+                <NFTCard nft={nft} ></NFTCard>
+              );
+            })
+          }
+        </div>
+        {pageKeys.length > 1 && (
+          <PaginationBar
+            currentPage={currentPage}
+            pageKeys={pageKeys}
+            onClickPage={onClickPage}
+            className="border-t"
+          />
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Home
--- pages/nftCard.jsx
+++ pages/nftCard.jsx
@@ -1,54 +1,63 @@
 import Swal from 'sweetalert2'
+import { ReactPaginate } from "react-paginate";
+import { useState } from "react";
+export const NFTCard = ({nft})=>{
+    const [isHovering, setIsHovering] = useState(false);
 
-export const NFTCard = ({ nft }) => {
-   
-  function Description(e) {
-    e.preventDefault();
-    Swal.fire({
-        title:nft.title,
-        text: nft.description,
-        imageUrl: nft.media[0].gateway,
-        imageWidth: 400,
-        imageHeight: 200,
-        imageAlt: 'Custom image',
-    })
-    //console.log('You clicked submit.'+nft.title);
-  }
-  function tokenId(e) {
-    e.preventDefault();
-    Swal.fire({
-        title:'tokenId',
-        text: nft.id.tokenId,
-        imageUrl: nft.media[0].gateway,
-        imageWidth: 400,
-        imageHeight: 200,
-        imageAlt: 'Custom image',
-    })
-    //console.log('You clicked submit.'+nft.title);
-  }
-  function contract(e) {
-    e.preventDefault();
-    Swal.fire({
-        title:'contract',
-        text: nft.contract.address,
-        imageUrl: nft.media[0].gateway,
-        imageWidth: 400,
-        imageHeight: 200,
-        imageAlt: 'Custom image',
-    })
-    //console.log('You clicked submit.'+nft.title);
-  }
-    return (
-        <div className="w-1/4 flex flex-col ">
-        <div className="rounded-md">
-            <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+ 
+    function Description(e) {
+      e.preventDefault();
+      Swal.fire({
+          title:nft.title,
+          text: nft.description,
+          imageUrl: nft.media[0].gateway,
+          imageWidth: 400,
+          imageHeight: 200,
+          imageAlt: 'Custom image',
+      })
+      //console.log('You clicked submit.'+nft.title);
+    }
+    function tokenId(e) {
+      e.preventDefault();
+      Swal.fire({
+          title:'tokenId',
+          text: nft.id.tokenId,
+          imageUrl: nft.media[0].gateway,
+          imageWidth: 400,
+          imageHeight: 200,
+          imageAlt: 'Custom image',
+      })
+      //console.log('You clicked submit.'+nft.title);
+    }
+    function contract(e) {
+      e.preventDefault();
+      Swal.fire({
+          title:'contract',
+          text: nft.contract.address,
+          imageUrl: nft.media[0].gateway,
+          imageWidth: 400,
+          imageHeight: 200,
+          imageAlt: 'Custom image',
+      })
+      //console.log('You clicked submit.'+nft.title);
+    }
+    return(
+        <div className="w-1/3 flex flex-col transition duration-500 hover:scale-110 border-2 border-black border-r-3">
+            <div className="rounded-md">
+                <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway}/>
+            </div>
+            <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
+                <div className="mr-3 ">
+                <h2 className="text-xl text-black">
+                   <b> Name: </b>{nft.title}
+                </h2>
+                      <center>
+                      <button className={"cursor-pointer shadow bg-green-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white hover:font-bold py-2 px-4 rounded"} onClick={Description}>Description</button>
+                      <button className={"cursor-pointer shadow bg-yellow-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white hover:font-bold py-2 px-4 rounded"} onClick={tokenId}>Token ID</button>
+                      <button className={"cursor-pointer shadow bg-orange-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white hover:font-bold py-2 px-4 rounded"}onClick={contract}>contract</button>
+                      </center>
+                </div>
+            </div>
         </div>
-        <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
-        <button className={" text-white bg-blue-400 "} onClick={Description}>Description</button>
-        <button className={" text-white bg-red-400 "} onClick={tokenId}>Token ID</button>
-        <button className={" text-white bg-green-400 "} onClick={contract}>contract</button>
-        </div>
-
-    </div>
-    )
+    );
 }
